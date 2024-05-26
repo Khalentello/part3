5ac606e8fb99e2344cced1b8918e83387d8b1c7b
Nicolas Barrero <nicofelipe26@hotmail.com>
2024-05-26 14:19:58 -0400

    End exercise 3.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 app = express();
+app.use(express.json());
 
 persons = [
   {
@@ -50,6 +51,28 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((p) => p.id !== id);
   response.status(204).end();
+});
+
+const generatedId = () => {
+  return Math.floor(Math.random() * 10000);
+};
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const person = {
+    id: generatedId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(persons);
 });
 
 const PORT = 3001;
